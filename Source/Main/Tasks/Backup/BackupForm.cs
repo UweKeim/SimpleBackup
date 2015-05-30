4f2c988ddd2d710f968bb870b8a963c3785178ca
Uwe Keim <uwe.keim@gmail.com>
2015-05-30 13:33:24 +0200

    Backup files

---
+++ Source/Main/Tasks/Backup/BackupForm.cs
@@ -0,0 +1,66 @@
+﻿namespace SimpleBackup.Main.Tasks.Backup
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Forms;
+    using Core;
+
+    public partial class BackupForm : Form
+    {
+        private string _sourceFolderPath;
+
+        public BackupForm()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialize(string sourceFolderPath)
+        {
+            _sourceFolderPath = sourceFolderPath;
+        }
+
+        private void BackupForm_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
+        }
+
+        private void BackupForm_Shown(object sender, EventArgs e)
+        {
+            processingBackgroundWorker.RunWorkerAsync();
+        }
+
+        private void processingBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Backuper.Backup(
+                _sourceFolderPath,
+                userState => processingBackgroundWorker.CancellationPending ? CancelMode.Cancel : CancelMode.Continue);
+        }
+
+        private void processingBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Hide();
+            if (e.Error != null) Program.HandleException(e.Error);
+
+            Application.Exit();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            buttonCancel.Enabled = false;
+            processingBackgroundWorker.CancelAsync();
+        }
+
+        private void BackupForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (processingBackgroundWorker.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+            else if (processingBackgroundWorker.IsBusy)
+            {
+                processingBackgroundWorker.CancelAsync();
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ Source/Main/Tasks/Backup/Backuper.cs
@@ -0,0 +1,51 @@
+﻿namespace SimpleBackup.Main.Tasks.Backup
+{
+    using System;
+    using System.IO;
+    using Core;
+    using Install;
+
+    internal static class Backuper
+    {
+        public static string BackupStorageBaseFolderPath
+        {
+            get { return Path.Combine(Installer.InstallationFolderPath, @"Backups"); }
+        }
+
+        public static void Backup(string sourceFolderPath, Cancelable cancelable)
+        {
+            if (sourceFolderPath.StartsWith(BackupStorageBaseFolderPath, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new Exception("Path must be outside of Simple Backup folder.");
+            }
+
+            var src = new DirectoryInfo(sourceFolderPath);
+            var dst = new DirectoryInfo(Path.Combine(BackupStorageBaseFolderPath, MakeUnique(src)));
+
+            if( dst.Exists)dst.Delete();
+            dst.Create();
+
+            CopyFilesRecursively(src, dst, cancelable);
+        }
+
+        public static string MakeUnique(FileSystemInfo src)
+        {
+            return string.Format(@"{0} ({1:X})", src.Name, src.FullName.GetHashCode());
+        }
+
+        public static void CopyFilesRecursively(DirectoryInfo source, DirectoryInfo target, Cancelable cancelable)
+        {
+            foreach (var dir in source.GetDirectories())
+            {
+                CancelHelper.CheckThrowCancel(cancelable);
+                CopyFilesRecursively(dir, target.CreateSubdirectory(dir.Name), cancelable);
+            }
+
+            foreach (var file in source.GetFiles())
+            {
+                CancelHelper.CheckThrowCancel(cancelable);
+                file.CopyTo(Path.Combine(target.FullName, file.Name));
+            }
+        }
+    }
+}
